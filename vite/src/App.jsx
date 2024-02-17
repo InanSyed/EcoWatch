b6d0c7173be56b2250788e8bf632568d56ebfcaa
jwlebert <j.lebert4402@gmail.com>
2024-02-17 15:51:18 -0500

    moved content into Content.jsx that is GOOD YAY LOL

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,30 +1,21 @@
 import React, { useState } from "react";
-import LoginScreen from "./LoginScreen";
-import SignUpScreen from "./SignUpScreen";
 
 import { Header } from './Header.jsx'
 import { Navbar } from './Navbar.jsx'
+import { Content } from './Content.jsx'
 
 const App = () => {
-  const [showSignUp, setShowSignUp] = useState(false);
+  const [page, changePage] = useState("empty");
 
-  const handleToggleSignUp = () => {
-    setShowSignUp(!showSignUp);
-  };
+  function c() {
+    changePage("login")
+  }
+
 
   return (
     <div className="h-screen flex align-center flex-col">
       <Header />
-
-      <div className="flex-1 grow">
-        <h1>ECOWatch</h1>
-        {showSignUp ? (
-          <SignUpScreen onCloseSignUp={handleToggleSignUp} />
-        ) : (
-          <LoginScreen onToggleSignUp={handleToggleSignUp} />
-        )}
-      </div>
-
+      <Content page={ page } />
       <Navbar />
     </div>
   );
