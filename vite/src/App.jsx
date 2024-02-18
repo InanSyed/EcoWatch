a14c06f434a75b6fdd83f8bd067f76d181bf3f3f
jwlebert <j.lebert4402@gmail.com>
2024-02-18 13:42:05 -0500

    fix

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './Header.jsx'
 import { Navbar } from './Navbar.jsx'
 import { Content, Login } from './Content.jsx'
