c2dfe4710079e5fb993e0ac233e8d675af5d7881
Inan <inansyed2@gmail.com>
2024-02-18 13:21:44 -0500

    recommit

--- vite/src/Content/LoginScreen.jsx
+++ vite/src/Content/LoginScreen.jsx
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBIToEBBXpngnJymATRKTx5BX7MdnvdXtk",
   authDomain: "ecowatch-c3a72.firebaseapp.com",
