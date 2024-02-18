f58cb323065de16c3b3ec2d6ddae31f0118d7cfa
jwlebert <j.lebert4402@gmail.com>
2024-02-18 09:57:37 -0500

    extracted firebase config into a json file

--- vite/src/content/LoginScreen.jsx
+++ vite/src/content/LoginScreen.jsx
@@ -8,16 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBIToEBBXpngnJymATRKTx5BX7MdnvdXtk",
-  authDomain: "ecowatch-c3a72.firebaseapp.com",
-  databaseURL: "https://ecowatch-c3a72-default-rtdb.firebaseio.com",
-  projectId: "ecowatch-c3a72",
-  storageBucket: "ecowatch-c3a72.appspot.com",
-  messagingSenderId: "361212644337",
-  appId: "1:361212644337:web:a79a5546f7ed8edfb95250",
-  measurementId: "G-WBY6W9Y0F7"
-};
+import firebaseConfig from "../../firebase.config.json";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
